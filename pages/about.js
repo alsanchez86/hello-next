ca62021636a62e410794338903f0e9d4e80bf02f
Angel <asancpiz@everis.com>
2020-01-08 16:33:52 +0100

    trying pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default () => (
+    <div>
+        <Link href="/">
+            <a>Index Page</a>
+        </Link>
+
+        <p>This is the about page</p>
+    </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-const Index = () => (
+import Link from 'next/link';
+
+export default () => (
     <div>
+        <Link href="/about">
+            <a title="About Page">About Page</a>
+        </Link>
+
         <p>Hello Next.js</p>
     </div>
 );
-
-export default Index;
