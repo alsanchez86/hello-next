059824b8ed7f304eb6efcd9c9a834aa90d3547d6
Angel <asancpiz@everis.com>
2020-01-09 14:21:47 +0100

    update post component

--- pages/post.js
+++ pages/post.js
@@ -1,13 +1,21 @@
 import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 
-export default () => {
+const Content = () => {
     const router = useRouter();
 
     return (
-        <Layout>
+        <>
             <h1>{router.query.title}</h1>
             <p>This is the blog post content.</p>
+        </>
+    );
+};
+
+export default () => {
+    return (
+        <Layout>
+            <Content/>
         </Layout>
     );
 };
