9ddc07fbe0b419f7e8025400688f020732be796a
Angel <asancpiz@everis.com>
2020-01-13 09:39:00 +0100

    try serverless nextjs

---
+++ pages/api/quote.js
@@ -0,0 +1,8 @@
+import quotes from "./quotes.json"
+
+export default (req, res) => {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    res
+        .status(200)
+        .json(quote);
+};
